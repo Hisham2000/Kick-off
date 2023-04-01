0c28ae1f48a616fbbd53435e23553557595671dd
Hisham_Anwar <hishamanwar72@gmail.com>
2023-04-01 19:44:43 +0200

    solve corossorigin

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [LoginController:: class, "login"]);
-Route::post('register',[RegisterController::class, "register"]);
+Route::post('login', [LoginController:: class, "login"])->middleware(["crossOrigin"]);
+Route::post('register',[RegisterController::class, "register"])->middleware(["crossOrigin"]);
 Route::get('roles', [RollesController::class, "gelAll"]);
 Route::get('area', [AreaController::class, "getAll"]);
 Route::get('clubs', [ClubsController::class, "getAll"]);
